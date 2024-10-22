2fdedab620676ab986a93d61b3699166589d6499
Rikesh <karmacharyar4@gmail.com>
2024-10-22 21:33:54 +0545

    route added

---
+++ views/theme/goodwill-cleaning/routes/route.js
@@ -0,0 +1,19 @@
+const homeController = require("../../../controllers/HomeController");
+
+
+// --------------FRONTEND ROUTES-------------------------------------
+router.get("/", homeController.getPage);
+router.get("/page/:slug", homeController.getStaticPage);
+router.get("/category/:slug", homeController.getCategoryListingPage);
+router.get("/post/:postId", homeController.getPostDetailPage);
+
+router.get('/contact', (req, res) => {
+  res.render('theme/goodwill-cleaning/pages/contactPage',{ showingpage: 'contact'}); 
+});
+
+router.get('/price', (req, res) => {
+  res.render('theme/goodwill-cleaning/pages/pricePage',{showingpage: 'price'}); 
+});
+router.get("/price", homeController.getPackage);
+
+// --------------FRONTEND ROUTES END---------------------------------
